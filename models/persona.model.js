69bdc4679c65e88bc7875292cd61c764ef4eb634
Diego Constantino <86428485+DiegoConstantino@users.noreply.github.com>
2021-07-20 19:07:19 -0500

    actiualizacion api

---
+++ models/persona.model.js
@@ -0,0 +1,18 @@
+import mongoose, {Schema} from "mongoose";
+
+const Personas = new Schema({
+    nombre_animal: { type: String, maxlenght: 50, required: true },
+    total_ganado: Number,
+    raza_ganado: String,
+    rango1: Number,
+    rango2: Number,
+    rango3: Number,
+
+    createdAt:{
+        type: Date, default: Date.now
+    }
+});
+
+const Persona= mongoose.model('persona', Personas);
+
+export default Persona
